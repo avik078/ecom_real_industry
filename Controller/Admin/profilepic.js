93b728d3df184c879caecfbb44c4c0d15bfebbe0
avik078 <avikb078@gmail.com>
2023-10-01 20:41:41 +0530

    multer added

--- Controller/Admin/profilepic.js
+++ Controller/Admin/profilepic.js
@@ -1,40 +1,9 @@
 
-const multer = require('multer');
 
-///////////////////////////////////////////////////////////////////
-const imageStorage = multer.diskStorage({
-    // Destination to store image     
-    destination: '../../uploads', 
-      filename: (req, file, cb) => {
-        const fileName = req.header.filename
-        console.log(file)
-          cb(null, file.fileName + '_' + Date.now() 
-             + path.extname(file.originalname))
-            // file.fieldname is name of the field (image)
-            // path.extname get the uploaded file extension
-    }
-});
-//////////////////////////////////////////////////////////////////
-const imageUpload = multer({
-    storage: imageStorage,
-    limits: {
-      fileSize: 1000000 // 1000000 Bytes = 1 MB
-    },
-    fileFilter(req, file, cb) {
-      if (!file.originalname.match(/\.(png|jpg)$/)) { 
-         // upload only png and jpg format
-         return cb(new Error('Please upload a Image'))
-       }
-     cb(undefined, true)
-  }
-}) 
+
 
 const  picUpload = async (req,res) => {
-
-    imageUpload(req)
-    res.status(200).send({data:"This pic upload route !"})
+  res.status(200).send({status: true , msg:"This pic upload route !! " ,data:req.file })
 }
 
 module.exports = {picUpload}
-
-
--- routes/adminpic/index.js
+++ routes/adminpic/index.js
@@ -2,9 +2,45 @@
 var express = require('express');
 var router = express.Router();
 var proPic = require("../../Controller/Admin/profilepic");
+const multer = require('multer');
+const path = require('path') ;
+///////////////////////////////////////////we run  multer as a middle ware here 
 
+/////////////////////////////////////////////////////////////////// storage
+const imageStorage = multer.diskStorage({
+  // Destination to store image     
+  destination: './uploads/adminpics',  // automatically starts from root path , does not support relative path
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now() 
+           + path.extname(file.originalname))
+          // file.fieldname is name of the field (image)
+          // path.extname get the uploaded file extension
+  }
+});
+////////////////////////////////////////////////////////////////// upload
+const imageUpload = multer({
+    storage: imageStorage,
+    limits: {
+      fileSize: 10000000 // 1000000 Bytes = 1 MB
+    },
+    fileFilter(req, file, cb) {
+      if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) { 
+         // upload only png and jpg format
+         return cb(new Error('Please upload a Image'))
+       }
+     cb(undefined, true)
+  }
+}) 
+///////////////////////////////////////////////////////////////////
+function routeCheck (req,res,next) {
+    console.log("multer route is hit")
+    next()
+}
 
-router.post("/picadm" ,proPic.picUpload)
+router.post("/picadm" ,routeCheck, imageUpload.array('image', 4) , proPic.picUpload , (error, req, res, next) => {   // 4  image atonce or imageUpload.single('image') for only one
+    res.status(400).send({ error: error.message })
+})
+
 
 module.exports = router;
 
